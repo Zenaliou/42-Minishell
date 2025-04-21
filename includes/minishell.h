3f7dde3f33ff0d9e4cb7134071b2b10be2bb2023
Zenaliou <leenicolas103@gmail.com>
2025-04-21 18:04:11 +0200

    did function env

--- includes/minishell.h
+++ includes/minishell.h
@@ -39,8 +39,8 @@ typedef struct s_cmd {
 // Tokenizing
 t_token	*new_token(char *value, t_token_type type);
 t_token *tokenize(char *input);
-void add_token(t_token **list, t_token *new);
-char *extract_word(char **input);
+void    add_token(t_token **list, t_token *new);
+char    *extract_word(char **input);
 
 // Parsing
 t_cmd *parse_tokens(t_token *tokens);
--- src/expansion.c
+++ src/expansion.c
@@ -6,18 +6,31 @@
 /*   By: niclee <niclee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 14:13:03 by niclee            #+#    #+#             */
-/*   Updated: 2025/04/21 14:24:04 by niclee           ###   ########.fr       */
+/*   Updated: 2025/04/21 17:59:46 by niclee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void expand_variables(t_token *tokens, char **env)
+void	expand_variables(t_token *tokens, char **env) // $HOME, $USER
 {
 	
 }
 
-char *expand_env(char *str, char **env)
+char	*expand_env(char *str, char **env) // $PATH, env
 {
-	
+	int i;
+	int len;
+
+	if (!str || !env)
+		return (NULL);
+	len = ft_strlen(str);
+	i = 0;
+	while(env[i])
+	{
+		if (ft_strncmp(env[i], str, len) == 0 && env[i][len] == '=')
+			return (ft_strdup(&env[i][len + 1]));
+		i++;
+	}
+	return (NULL);
 }
