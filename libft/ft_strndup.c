504bfc5051f667e968c7d4ce555e5678839b9444
Zenaliou <leenicolas103@gmail.com>
2025-05-16 10:55:27 +0200

    libft norminette

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -5,22 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: niclee <niclee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/11 10:54:37 by niclee            #+#    #+#             */
-/*   Updated: 2025/04/11 14:21:47 by niclee           ###   ########.fr       */
+/*   Created: 2025/05/16 10:52:19 by niclee            #+#    #+#             */
+/*   Updated: 2025/05/16 10:54:15 by niclee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-char *ft_strndup(const char *s, size_t n)
+char	*ft_strndup(const char *s, size_t n)
 {
-    char *dup;
-    size_t len = strnlen(s, n);
+	char	*dup;
+	size_t	len;
 
-    dup = malloc(len + 1);
-    if (!dup)
-        return (NULL);
-    strncpy(dup, s, len);
-    dup[len] = '\0';
-    return (dup);
+	len = strnlen(s, n); // forbidden function
+	dup = malloc(len + 1);
+	if (!dup)
+		return (NULL);
+	strncpy(dup, s, len);
+	dup[len] = '\0';
+	return (dup);
 }
