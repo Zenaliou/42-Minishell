056855c7b093bde33c7869ae796f3eccd79fb0e7
Gamabuntoo <gamabuntoo@gmail.com>
2025-07-02 01:35:40 +0200

    Crash à cause d'un printf sans environnement

--- src/Exec/Builtins/env.c
+++ src/Exec/Builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: gule-bat <gule-bat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 17:45:19 by gule-bat          #+#    #+#             */
-/*   Updated: 2025/06/27 17:58:20 by gule-bat         ###   ########.fr       */
+/*   Updated: 2025/07/02 01:32:01 by gule-bat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,12 @@ int	search_env(char *line, char *baseline)
 	char	*cpy;
 	
 	i = 0;
+	cpy = NULL;
+	if (!line | !baseline)
+		return (-1);
 	cpy = ft_strdup(baseline);
 	if (!cpy)
-		return (-1);
+		return (free(cpy), -1);
 	while (line[i] && line[i] != '=')
 		i++;
 	cpy[i] = '\0';
@@ -53,14 +56,13 @@ void	builtin_unset(char **argv, t_env **envi)
 	//faire l'env de base de env -i avec pwd shlvl etc...
 	tmp = NULL;
 	head = envi;
-	if (!head)
-		return ;
 
+	if (!envi | !(*envi)->full)
+		return ;
 	(void)argv;
 	while ((*head)->next != NULL && search_env(argv[1], (*head)->next->full) == -1)
 		head = &(*head)->next;
 	head = &(*head)->next;
-	printf("deleted node = %s\n", (*head)->next->full);
 	if (*head && search_env(argv[1], (*head)->full) == 1)
 	{
 		tmp = *head;
